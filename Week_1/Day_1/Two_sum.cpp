6c42dcd340864598aa398e32b60112cc33639bc1
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-06-01 17:20:39 +0530

    Update Two_sum.cpp

--- Week_1/Day_1/Two_sum.cpp
+++ Week_1/Day_1/Two_sum.cpp
@@ -1,1 +1,18 @@
-paste code
+// LeetCode: Two Sum
+https://leetcode.com/problems/two-sum/submissions/2019058418/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      int i,j;
+        for( i=0;i<nums.size();i++){
+            for( j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    return{i,j};
+                }
+            }
+
+        }
+        return {i,j};
+       
+    }
+};
