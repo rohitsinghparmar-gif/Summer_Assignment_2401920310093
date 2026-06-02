2cdd4787b40613d090247b3dada33da841721f57
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-06-02 21:23:45 +0530

    Create Maximum_subarray.cpp

---
+++ Week_1/Day_2/Maximum_subarray.cpp
@@ -0,0 +1,23 @@
+Leetcode: Maximum Subarray
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = INT_MIN;
+
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i; j < nums.size(); j++) {
+
+                int sum = 0;
+
+                for(int k = i; k <= j; k++) {
+                    sum += nums[k];
+                }
+
+                maxSum = max(maxSum, sum);
+            }
+        }
+
+        return maxSum;
+        
+    }
+};
