0b58dca82facdfe68422b4bf7ce697b7b921f4d7
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-06-03 15:50:38 +0530

    Update Move_zeroes.cpp

--- Week_1/Day_3/Move_zeroes.cpp
+++ Week_1/Day_3/Move_zeroes.cpp
@@ -1,1 +1,23 @@
+LeetCode: Move Zeroes
+https://leetcode.com/problems/move-zeroes/submissions/2015116141/
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        vector<int>arr;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                arr.push_back(nums[i]);
+            }
+           
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                arr.push_back(0);
 
+            }
+            }
+         nums=arr;
+     
+                
+    }
+};
