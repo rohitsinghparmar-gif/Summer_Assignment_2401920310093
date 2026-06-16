5afe9b7f91c8f17e5c870f7d0c97bd67edaebd2c
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-06-16 20:53:26 +0530

    Create palindrome_linked_list.cpp

---
+++ Week_3/Day_2/palindrome_linked_list.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL||head->next==NULL){
+            return true;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL&&fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* prev=NULL;
+        ListNode* curr=slow;
+        while(curr!=NULL){
+            ListNode* next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        ListNode* first=head;
+        ListNode* second=prev;
+        while(second!=NULL){
+            if(first->val!=second->val){
+                return false;}
+                first=first->next;
+                second=second->next;
+            }
+            return true;
+        
+    }
+    
+};
