66494c912a5a80e46f8c57741992712429faf37f
rohitsinghparmar-gif <parmarrohitsingh4@gmail.com>
2026-06-23 19:25:47 +0530

    Create binary_tree_level_order_traversal.cpp

---
+++ Week_4/Day_2/binary_tree_level_order_traversal.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==NULL){
+            return ans ;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size=q.size();
+            vector<int>level;
+            for(int i=0;i<size;i++){
+            TreeNode* curr=q.front();
+            q.pop();
+            level.push_back(curr->val);
+            if(curr->left!=NULL){
+                q.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                q.push(curr->right);
+            }
+          }ans.push_back(level);}
+        
+        return ans;
+        
+    }
+};
